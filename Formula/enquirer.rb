80da1c7191ecdd7fafa9dfc1f0029fd8eb755a76
buddy <buddy@buddy.works>
2020-01-17 17:56:38 +0000

    homebrew: v0.1.0

--- Formula/enquirer.rb
+++ Formula/enquirer.rb
@@ -1,14 +1,14 @@
 class Enquirer < Formula
-  version "0.0.1"
+  version "0.1.0"
   desc "Command Line Utility for Stylish Interactive Prompts"
   homepage "https://github.com/termapps/enquirer"
 
   if OS.mac?
     url "https://github.com/termapps/enquirer/releases/download/#{version}/enquirer-#{version}-x86_64-apple-darwin.zip"
-    sha256 "c700385a3a470fcc137e2bc71d8c1a2b3ff5723a7da0cb081859b18036076d35" # mac
+    sha256 "7f260a6ca70fe27f8d20a56cc31b0462b32a63c5acbf65059ed07b2b31f622c3" # mac
   elsif OS.linux?
     url "https://github.com/termapps/enquirer/releases/download/#{version}/enquirer-#{version}-x86_64-unknown-linux-gnu.zip"
-    sha256 "2c900c315a059dbd3079f5be781ef674bf0c124d1254cc986ba1c127ae4fa406" # linux
+    sha256 "c90bffe2b0feb56402009f3c87d05c6e0d9cb0c5f8e9052d4661627dac00bbf5" # linux
   end
 
   def install
